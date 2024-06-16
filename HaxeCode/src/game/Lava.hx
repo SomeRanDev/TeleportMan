498892a5936b25ce97fa20e29ece2861b24d38ce
Robert Borghese <28355157+SomeRanDev@users.noreply.github.com>
2024-06-16 02:39:32 -0400

    Block out Level6, make dynamic lava levels

---
+++ HaxeCode/src/game/Lava.hx
@@ -0,0 +1,44 @@
+package game;
+
+import godot.*;
+
+import game.ShiftingNoiseTexture;
+
+using game.MacroHelpers;
+using game.NodeHelpers;
+
+class Lava extends ShiftingNoiseTexture {
+	@:export public var initialLavalLevel = -35.0;
+
+	var currentLavaLevel = -35.0;
+	var targetLavaLevel = -35.0;
+
+	public override function _ready() {
+		super._ready();
+
+		setTargetLavaLevel(initialLavalLevel);
+		setCurrentLavaLevel(initialLavalLevel);
+	}
+
+	public function setTargetLavaLevel(level: Float) {
+		targetLavaLevel = level;
+	}
+
+	public override function _process(delta: Float) {
+		super._process(delta);
+
+		if(currentLavaLevel != targetLavaLevel) {
+			setCurrentLavaLevel(Godot.move_toward(currentLavaLevel, targetLavaLevel, delta * 10.0));
+		}
+	}
+
+	function setCurrentLavaLevel(level: Float) {
+		currentLavaLevel = level;
+
+		position.y = level;
+
+		final meshInstance = this.get_scene_node("LevelWalls").as(MeshInstance3D);
+		final shaderMaterial = meshInstance.get_surface_override_material(0).as(ShaderMaterial);
+		shaderMaterial.set_shader_parameter("lava_level", level);
+	}
+}
